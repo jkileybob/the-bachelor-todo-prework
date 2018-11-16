698d7f8d2f947dfa732309081859226e436f386d
Kiley Bobbitt <jkileybob@aol.com>
2018-11-16 01:30:58 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,7 +22,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestant_array|
+    contestant_array.each do |input|
+      if input == "hometown"
+        counter += 1
+      end
+    end
+  end
+  return counter      
 end
 
 def get_occupation(data, hometown)
