f665eed3922f4121b6ddae4de0bc6509ee88892b
Kiley Bobbitt <jkileybob@aol.com>
2018-11-16 01:35:28 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -24,8 +24,8 @@ end
 def count_contestants_by_hometown(data, hometown)
   counter = 0
   data.each do |season, contestant_array|
-    contestant_array.each do |hash|
-      if hash == "hometown"
+    contestant_array.each do |hometown|
+      if hometown == "hometown"
         counter += 1
       end
     end
